7e17afa221bc153d908accbc443859ef8c43f825
trentjohnson <zookmurnig@gmail.com>
2026-03-15 08:34:43 -0400

    add get member

--- server/controllers/memberController.js
+++ server/controllers/memberController.js
@@ -1,4 +1,8 @@
 import Member from '../models/Member.js';
+import Vehicle from '../models/Vehicle.js';
+import Transaction from '../models/Transaction.js';
+import Activity from '../models/Activity.js';
+import Subscription from '../models/Subscription.js';
 
 export const getMembers = async (req, res) => {
   const members = await Member.find();
@@ -29,4 +33,28 @@ export const deleteMember = async (req, res) => {
   const member = await Member.findByIdAndDelete(req.params.id);
   if (!member) return res.status(404).json({ message: 'Member not found' });
   res.json({ message: 'Member deleted' });
+};
+
+export const getMemberVehicles = async (req, res) => {
+  const vehicles = await Vehicle.find({ member_id: req.params.id });
+  res.json(vehicles);
+};
+
+export const getMemberSubscriptions = async (req, res) => {
+  const subscriptions = await Subscription.find({ member_id: req.params.id })
+    .populate('plan_id', 'plan_name monthly_price')
+    .populate('vehicle_id', 'license_plate make_model');
+  res.json(subscriptions);
+};
+
+export const getMemberTransactions = async (req, res) => {
+  const transactions = await Transaction.find({ member_id: req.params.id })
+    .sort({ timestamp: -1 });
+  res.json(transactions);
+};
+
+export const getMemberActivities = async (req, res) => {
+  const activities = await Activity.find({ member_id: req.params.id })
+    .sort({ timestamp: -1 });
+  res.json(activities);
 };
--- server/routes/members.js
+++ server/routes/members.js
@@ -5,12 +5,20 @@ import {
   createMember,
   updateMember,
   deleteMember,
+  getMemberVehicles,
+  getMemberSubscriptions,
+  getMemberTransactions,
+  getMemberActivities,
 } from '../controllers/memberController.js';
 
 const router = express.Router();
 
 router.get('/', getMembers);
 router.get('/:id', getMember);
+router.get('/:id/vehicles', getMemberVehicles);
+router.get('/:id/subscriptions', getMemberSubscriptions);
+router.get('/:id/transactions', getMemberTransactions);
+router.get('/:id/activities', getMemberActivities);
 router.post('/', createMember);
 router.put('/:id', updateMember);
 router.delete('/:id', deleteMember);
